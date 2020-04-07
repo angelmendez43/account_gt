e4908041ebefe036171e88ff5539aca6195778fd
jefferson <jeffer.leo93@gmail.com>
2020-04-07 17:51:23 -0600

    account_gt

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "account_gt",
+
+    'summary': """ Conta extra para guate """,
+
+    'description': """
+        Conta extra para guate
+    """,
+
+    'author': "JS",
+    'website': "",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+}
