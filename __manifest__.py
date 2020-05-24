f8e967991c201d6514601d8616eb0354c84930da
jefferson <jeffer.leo93@gmail.com>
2020-05-23 21:58:46 -0600

    Liquidacion

--- __manifest__.py
+++ __manifest__.py
@@ -19,6 +19,9 @@
     'data': [
         'views/report.xml',
         'views/res_partner_views.xml',
+        'views/account_gt_views.xml',
+        'views/account_move_views.xml',
+        'views/account_payment_view.xml',
         'report/reporte_libro_compras_views.xml',
         'report/reporte_libro_bancos_views.xml',
         'report/reporte_libro_conciliacion_bancaria_views.xml',
@@ -26,5 +29,6 @@
         'wizard/libro_bancos_wizard_views.xml',
         'wizard/libro_conciliacion_bancaria_wizard_views.xml',
         'wizard/conciliacion_bancaria_wizard_views.xml',
+        'data/ir_sequence_data.xml',
     ],
 }
--- models/account_move.py
+++ models/account_move.py
@@ -1,6 +1,13 @@
 from odoo import api, fields, models, tools, _
 from odoo.modules import get_module_resource
 
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    liquidacion_id = fields.Many2one('account_gt.liquidacion','Liquidacion')
+
+
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
