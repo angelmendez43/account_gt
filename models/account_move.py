e8c885870c2575bfd0da93e9e2e150a9de034c6e
jeffrepo <jeffer.leo93@gmail.com>
2021-03-04 08:52:30 -0600

    move

--- models/account_move.py
+++ models/account_move.py
@@ -1,20 +1,31 @@
 from odoo import api, fields, models, tools, _
 from odoo.modules import get_module_resource
+from odoo.release import version_info
 import logging
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    @api.onchange('journal_id')
-    def onchange_tipo_factura(self):
-        tipo = False
-        logging.warn(self)
-        if self.type in ['in_invoice','in_refund']:
-            tipo = 'compra'
-        if self.type in ['out_invoice','out_refund']:
-            tipo = 'venta'
-        logging.warn(tipo)
-        self.tipo_factura = tipo
+    if version_info[0] == 13:
+        @api.onchange('journal_id')
+        def onchange_tipo_factura(self):
+            tipo = False
+            if self.type in ['in_invoice','in_refund']:
+                tipo = 'compra'
+            if self.type in ['out_invoice','out_refund']:
+                tipo = 'venta'
+            logging.warn(tipo)
+            self.tipo_factura = tipo
+    else:
+        @api.onchange('journal_id')
+        def onchange_tipo_factura(self):
+            tipo = False
+            if self.move_type in ['in_invoice','in_refund']:
+                tipo = 'compra'
+            if self.move_type in ['out_invoice','out_refund']:
+                tipo = 'venta'
+            logging.warn(tipo)
+            self.tipo_factura = tipo
 
     liquidacion_id = fields.Many2one('account_gt.liquidacion','Liquidacion')
     tipo_factura = fields.Selection([('venta','Venta'),('compra', 'Compra o Bien'), ('servicio', 'Servicio'),('varios','Varios'), ('combustible', 'Combustible'),('importacion', 'Importación'),('exportacion','Exportación')],
