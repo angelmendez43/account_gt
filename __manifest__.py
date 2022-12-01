b1942e470b30d4a3dcb8ac578b770b82e168a977
jeffrepo <jeffer.leo93@gmail.com>
2022-11-30 22:52:46 -0600

    Libro mayor GT

--- __manifest__.py
+++ __manifest__.py
@@ -12,9 +12,9 @@
     'website': "",
 
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '0.2',
 
-    'depends': ['account','base'],
+    'depends': ['account','account_reports','base'],
 
     'data': [
         'security/ir.model.access.csv',
@@ -25,6 +25,7 @@
         'views/account_payment_view.xml',
         'views/product_template_views.xml',
         'views/account_journal_views.xml',
+        'views/report_financial.xml',
         'report/reporte_libro_compras_views.xml',
         'report/reporte_libro_ventas_views.xml',
         'report/reporte_libro_bancos_views.xml',
---
+++ models/account_general_ledger.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models, fields, api, _
+from odoo.tools.misc import format_date, DEFAULT_SERVER_DATE_FORMAT
+from datetime import timedelta
+
+
+class AccountGeneralLedgerReport(models.AbstractModel):
+    _inherit = "account.general.ledger"
+
+    @api.model
+    def _get_columns_name(self, options):
+        res = super(AccountGeneralLedgerReport, self)._get_columns_name(options)
+        for c in res:
+            if c['name'] == '':
+                c['name'] = 'Cuenta'
+            if c['name'] == 'Comunicación':
+                c['name'] = 'Descripción'    
+            if c['name'] == 'Balance':
+                c['name'] = 'Saldo Final'  
+        return res
