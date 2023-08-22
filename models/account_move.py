3a90ffd0165bb30e56f141ff21f940d6294403ce
jeffrepo <jeffer.leo93@gmail.com>
2023-08-22 11:06:19 -0600

    update fexp

--- models/account_move.py
+++ models/account_move.py
@@ -34,6 +34,27 @@ class AccountMove(models.Model):
     ('importacion', 'Importación'),('exportacion','Exportación'),
     ('factura_especial', 'Factura especial')],
         string="Tipo de factura")
+    consignatario_destinatario_id = fields.Many2one('res.partner', string="Consignatario o Destinatario")
+    codigo_consignatario_destinatario = fields.Char('Código consignatario o destinatario')
+    comprador_id = fields.Many2one('res.partner', string="Comprador")
+    exportador_id = fields.Many2one('res.partner', string="Exportador")
+    direccion_comprador = fields.Char('Dirección comprador')
+    otra_referencia = fields.Char('Otra referencia')
+    incoterm_exp = fields.Selection([('EXW','EXW'),('FCA', 'FCA'),
+    ('FAS', 'FAS'), ('FOB', 'FOB'),
+    ('CFR','CFR'), ('CIF', 'CIF'),
+    ('CPT', 'CPT'),('CIP','CIP'),
+    ('DDP','DDP'),('DAP','DAP'),
+    ('DPU','DPU'),('ZZZ','ZZZ')],
+        string="Intocerm exportación")
+    nombre_exportador = fields.Char('Nombre exportador')
+    codigo_exportador = fields.Char('Código exportador')
+
+    @api.onchange('consignatario_destinatario_id')
+    def _onchange_consignatario_destinatario_id(self):
+        if self.consignatario_destinatario_id:
+            self.comprador_id = self.partner_id.id
+            self.exportador_id = self.partner_id.id
 
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
