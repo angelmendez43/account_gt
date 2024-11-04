1071c900a13a197bce93865f28d2b074b3c2bdce
jefferson Silva <8306339+jeffrepo@users.noreply.github.com>
2024-11-04 17:04:56 -0600

    Update libro_compras_report.py

--- report/libro_compras_report.py
+++ report/libro_compras_report.py
@@ -140,14 +140,33 @@ class LibroCompras(models.AbstractModel):
                             factura = compra.fel_serie
                             documento = compra.fel_numero                     
                         else:
-                            if factura == False and documento == False:
-                                if compra.ref:
-                                    if '-' in compra.ref:
-                                        factura = compra.ref.split('-')[0]
-                                        documento = compra.ref.split('-')[1]
-                                    elif '/' in compra.ref:
-                                        factura = compra.ref.split('/')[0]
-                                        documento = compra.ref.split('/')[1]
+                            if compra.ref:
+                                if '-' in compra.ref:
+                                    fel_serie = compra.ref.split('-')[0]
+                                    fel_numero = compra.ref.split('-')[1]
+                                elif '/' in compra.ref:
+                                    fel_serie = compra.ref.split('/')[0]
+                                    fel_numero = compra.ref.split('/')[1]
+                                elif ' ' in compra.ref:
+                                    fel_serie = compra.ref.split(' ')[0]
+                                    fel_numero = compra.ref.split(' ')[1]
+                                else:
+                                    fel_serie = ""
+                                    fel_numero = ""                                
+                            else:
+                                if compra.payment_reference:
+                                    if '-' in compra.payment_reference:
+                                        fel_serie = compra.payment_reference.split('-')[0]
+                                        fel_numero = compra.payment_reference.split('-')[1]
+                                    elif '/' in compra.payment_reference:
+                                        fel_serie = compra.payment_reference.split('/')[0]
+                                        fel_numero = compra.payment_reference.split('/')[1]
+                                    elif ' ' in compra.payment_reference:
+                                        fel_serie = compra.payment_reference.split(' ')[0]
+                                        fel_numero = compra.payment_reference.split(' ')[1]
+                                    else:
+                                        fel_serie = ""
+                                        fel_numero = ""
 
                         documentos_operados += 1
                         if compra.journal_id:
