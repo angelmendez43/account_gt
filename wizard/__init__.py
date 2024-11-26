436fc1aa4a98fb1046c4255e1438998a0ae99b4e
jefferson Silva <8306339+jeffrepo@users.noreply.github.com>
2024-11-26 12:59:09 -0600

    Conciliacion manual

--- wizard/__init__.py
+++ wizard/__init__.py
@@ -6,4 +6,4 @@ from . import libro_ventas_wizard
 from . import libro_bancos_wizard
 from . import top_diez_proveedores_wizard
 from . import libro_conciliacion_bancaria_wizard
-# from . import conciliacion_bancaria_wizard
+from . import conciliacion_bancaria_wizard
